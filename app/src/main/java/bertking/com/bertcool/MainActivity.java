5b9e40fab5a9a9c7baeb9f869ce5b43b69601852
DESKTOP-OSN7PRF\king <kingcheeng163@gmail.com>
2018-04-24 23:00:30 +0800

    Create a base Application

---
+++ app/src/main/java/bertking/com/bertcool/MainActivity.java
@@ -0,0 +1,17 @@
+package bertking.com.bertcool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author king
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
